cdf211cecc215ae379491b3a24857d4c09d82658
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-10 23:42:47 +0200

    layout set

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-import flask from Flask
+from flask import Flask, render_template
 
 #store the flask instance 
-
+#instantiating the flask object
 app = Flask(__name__)
 
 
 #url to view website
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return "okay"
+
+
+if __name__ == "__main__": 
+    app.run(debug = True)
